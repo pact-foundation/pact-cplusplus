c5e59d1948d71a1b06eee85ada79ecd57e3d74d0
Yousaf Nabi <you@saf.dev>
2026-09-08 11:40:10 +0100

    chore: update release wf to conan 2.0

--- consumer/conanfile.py
+++ consumer/conanfile.py
@@ -1,8 +1,9 @@
 import os
 
 from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
-from conan.tools.files import copy
+from conan.tools.files import copy, rename
 
 
 class PactcppconsumerConan(ConanFile):
@@ -15,7 +16,8 @@ class PactcppconsumerConan(ConanFile):
     package_type = "static-library"
     settings = "os", "compiler", "build_type", "arch"
     options = {"fPIC": [True, False]}
-    default_options = {"fPIC": True}
+    # Only boost/throw_exception.hpp is used, so no compiled Boost libraries are needed
+    default_options = {"fPIC": True, "boost/*:header_only": True}
     exports_sources = "CMakeLists.txt", "src/*", "include/*", "test/*"
 
     def config_options(self):
@@ -24,9 +26,9 @@ class PactcppconsumerConan(ConanFile):
 
     def requirements(self):
         self.requires("nlohmann_json/3.11.3")
+        self.requires("boost/1.86.0", transitive_headers=True)
 
     def build_requirements(self):
-        self.requires("boost/1.83.0", transitive_headers=True)
         self.test_requires("gtest/1.15.0")
         self.test_requires("libcurl/8.10.1")
 
@@ -38,12 +40,27 @@ class PactcppconsumerConan(ConanFile):
         tc = CMakeToolchain(self)
         tc.cache_variables["PactUseConan"] = True
         tc.cache_variables["PactBuildTests"] = False
+        # The pact_ffi binaries are downloaded outside of conan
+        ffi_root = os.environ.get("PACT_FFI_ROOT")
+        if not ffi_root:
+            raise ConanInvalidConfiguration("PACT_FFI_ROOT must point at the pact_ffi installation")
+        tc.cache_variables["PACT_FFI_ROOT"] = ffi_root.replace("\\", "/")
+        tc.cache_variables["PACT_FFI_VERSION"] = os.environ.get("PACT_FFI_VERSION", "v0.5.0")
         tc.generate()
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
+
+    def _ffi_files(self):
+        arch = "aarch64" if str(self.settings.arch) in ("armv8", "arm64") else "x86_64"
+        if self.settings.os == "Macos":
+            return {f"libpact_ffi-macos-{arch}.a": "libpact_ffi.a"}
+        if self.settings.os == "Windows":
+            return {f"pact_ffi-windows-{arch}.lib": "pact_ffi.lib"}
+        libc = "-musl" if os.environ.get("PACT_FFI_LIBC") == "musl" else ""
+        return {f"libpact_ffi-linux-{arch}{libc}.a": "libpact_ffi.a"}
 
     def package(self):
         copy(self, "*.h",
@@ -55,8 +72,25 @@ class PactcppconsumerConan(ConanFile):
         copy(self, "*.dll", src=self.build_folder,
              dst=os.path.join(self.package_folder, "bin"), keep_path=False)
 
+        # consumer.h includes <pact.h>, so the FFI headers and archive ship with the package
+        ffi_root = os.environ["PACT_FFI_ROOT"]
+        copy(self, "*.h",
+             src=os.path.join(ffi_root, "include"),
+             dst=os.path.join(self.package_folder, "include"))
+        for source, target in self._ffi_files().items():
+            copy(self, source, src=os.path.join(ffi_root, "lib"),
+                 dst=os.path.join(self.package_folder, "lib"), keep_path=False)
+            rename(self, os.path.join(self.package_folder, "lib", source),
+                   os.path.join(self.package_folder, "lib", target))
+
     def package_info(self):
-        self.cpp_info.libs = ["pact-cpp-consumer"]
+        self.cpp_info.libs = ["pact-cpp-consumer", "pact_ffi"]
         if self.settings.os == "Windows":
             self.cpp_info.system_libs = ["ws2_32", "userenv", "crypt32",
                                          "secur32", "dnsapi", "ncrypt", "ntdll"]
+        elif self.settings.os == "Macos":
+            self.cpp_info.frameworks = ["CoreFoundation", "Security",
+                                        "SystemConfiguration", "IOKit"]
+            self.cpp_info.system_libs = ["bz2", "lzma", "iconv"]
+        else:
+            self.cpp_info.system_libs = ["pthread", "dl", "m", "bz2", "lzma"]
